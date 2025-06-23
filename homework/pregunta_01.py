1a8e984fe35c62107f62ef96686f756a94a6bc24
mafe <mvalenzuela@unal.edu.co>
2025-06-22 23:11:49 -0500

    Resultados lab8

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,11 +3,125 @@
 Escriba el codigo que ejecute la accion solicitada.
 """
 
+import os
+import glob
+import pandas as pd
+import matplotlib
+matplotlib.use("Agg")  # Fuerza uso de backend no gráfico (ideal para entornos sin GUI)
+import matplotlib.pyplot as plt
 
 def pregunta_01():
+    ruta_archivos = "docs"
+
+    if os.path.exists(ruta_archivos):
+        for file in glob.glob(f"{ruta_archivos}/*"):
+            os.remove(file)
+        os.rmdir(ruta_archivos)
+    os.makedirs(ruta_archivos)
+
+    def load_data():
+        df = pd.read_csv("files/input/shipping-data.csv")
+        return df
+
+    def create_visual_for_shipping_per_warehouse(df):
+        df = df.copy()
+        plt.figure()
+        counts = df.Warehouse_block.value_counts()
+        counts.plot.bar(
+            title="Shipping per Warehouse", 
+            xlabel="Warehouse block", 
+            ylabel="Record Count", 
+            color="tab:blue", 
+            fontsize=8)
+        plt.gca().spines["top"].set_visible(False)
+        plt.gca().spines["right"].set_visible(False)
+        plt.savefig(f"{ruta_archivos}/shipping_per_warehouse.png")
+
+    def create_visual_for_mode_of_shipment(df):
+        df = df.copy()
+        plt.figure()
+        counts = df.Mode_of_Shipment.value_counts()
+        counts.plot.pie(
+            title="Mode of shipment",
+            wedgeprops=dict(width=0.35), 
+            ylabel="", 
+            colors=["tab:blue", "tab:orange", "tab:green"])
+        plt.savefig(f"{ruta_archivos}/mode_of_shipment.png")
+    
+    def create_visual_for_average_customer_rating(df):
+        df = df.copy()
+        plt.figure()
+        df = (
+            df[["Mode_of_Shipment", "Customer_rating"]]
+              .groupby("Mode_of_Shipment")
+              .describe()
+        )
+        df.columns = df.columns.droplevel()
+        df = df[["mean", "min", "max"]]
+        plt.barh(
+            y=df.index.values, 
+            width=df["max"].values - 1, 
+            left=df["min"].values,
+            height=0.9, 
+            color="lightgray", 
+            alpha=0.8)
+        colors=["tab:green" if value >= 3.0 else "tab:orange" for value in df["mean"].values]
+        plt.barh(
+            y=df.index.values, 
+            width=df["mean"].values - 1, 
+            left=df["min"].values, 
+            height=0.5, 
+            color=colors, 
+            alpha=1.0
+        )
+        plt.title("Average Customer Rating")
+        plt.gca().spines["left"].set_color("gray")
+        plt.gca().spines["bottom"].set_color("gray")
+        plt.gca().spines["top"].set_visible(False)
+        plt.gca().spines["right"].set_visible(False)
+        plt.savefig(f"{ruta_archivos}/average_customer_rating.png")
+        
+    def create_visual_for_weight_distribution(df):
+        df = df.copy()
+        plt.figure()
+        df.Weight_in_gms.plot.hist(title="Shipped Weight Distribution", color="tab:orange", edgecolor="white")
+        plt.gca().spines["top"].set_visible(False)
+        plt.gca().spines["right"].set_visible(False)
+        plt.savefig(f"{ruta_archivos}/weight_distribution.png")
+
+    def generate_html():
+        contenido = '''<!DOCTYPE html>
+        <html>
+            <body>
+                <h1>Shipping Dashboard Example</h1>
+                <div style="width:45%;float:left">
+                    <img src="shipping_per_warehouse.png" alt="Fig 1">
+                    <img src="mode_of_shipment.png" alt="Fig 2">
+                </div>
+                <div style="width:45%;float:left">
+                    <img src="average_customer_rating.png" alt="Fig 3">
+                    <img src="weight_distribution.png" alt="Fig 4">
+                </div>
+            </body>
+        </html>
+        '''
+        with open(f"{ruta_archivos}/index.html", "w") as archivo:
+            archivo.write(contenido)
+
+    #llamado final de metodos para generar gráficos
+    df = load_data()
+    create_visual_for_shipping_per_warehouse(df)
+    create_visual_for_mode_of_shipment(df)
+    create_visual_for_average_customer_rating(df)
+    create_visual_for_weight_distribution(df)
+    generate_html()
+
+
+
+
     """
-    El archivo `files//shipping-data.csv` contiene información sobre los envios
-    de productos de una empresa. Cree un dashboard estático en HTML que
+    El archivo `files//shipping-data.csv` contiene informaciÃ³n sobre los envios
+    de productos de una empresa. Cree un dashboard estÃ¡tico en HTML que
     permita visualizar los siguientes campos:
 
     * `Warehouse_block`
@@ -35,3 +149,5 @@ def pregunta_01():
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
+
+pregunta_01()
